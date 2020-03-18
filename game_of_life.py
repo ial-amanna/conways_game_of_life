40715cf1295d6f944a97b740feb13a5fc7a7b878
ial-amanna <l.annamalai98@gmail.com>
2020-03-18 18:03:20 +0530

    added the code

---
+++ game_of_life.py
@@ -0,0 +1,59 @@
+import numpy as np
+import time
+import argparse
+
+class Board(object):
+   def __init__(self, size, seed = 'Random'):
+      if seed == 'Random':
+         self.state = np.random.randint(2, size = size)
+      self.engine = Engine(self)
+      self.iteration = 0
+   def display_at_prompt(self):
+      i = self.iteration
+      while i<10:
+         i += 1
+         self.engine.applyRules()
+         print('Life Cycle: {} Birth: {} Survive: {}'.format(i, self.engine.born, self.engine.survived))
+         yield self
+
+
+
+class Engine(object):
+   def __init__(self, board):
+      self.state = board.state
+   def countNeighbors(self):
+      state = self.state
+      n = (state[0:-2,0:-2] + state[0:-2,1:-1] + state[0:-2,2:] +
+          state[1:-1,0:-2] + state[1:-1,2:] + state[2:,0:-2] +
+          state[2:,1:-1] + state[2:,2:])
+      return n
+   def applyRules(self):
+      n = self.countNeighbors()
+      state = self.state
+      birth = (n == 3) & (state[1:-1,1:-1] == 0)
+      survive = ((n == 2) | (n == 3)) & (state[1:-1,1:-1] == 1)
+      state[...] = 0
+      state[1:-1,1:-1][birth | survive] = 1
+      born = np.sum(birth)
+      self.born = born
+      survived = np.sum(survive)
+      self.survived = survived
+      return state
+
+#-------------------------------------------------------------------------
+
+def main():
+   ap = argparse.ArgumentParser(add_help = False) # Intilialize Argument Parser
+   ap.add_argument('height', help = 'Board Height', default = 256)
+   ap.add_argument('width', help = 'Board Width', default = 256)
+   args = vars(ap.parse_args()) # Gather Arguments
+   bHeight = int(args['height'])
+   bWidth = int(args['width'])
+   board = Board((bHeight,bWidth))
+   for _ in board.display_at_prompt():
+      time.sleep(1)
+      pass
+#-------------------------------------------------------------------------
+
+if __name__ == '__main__':
+   main()
